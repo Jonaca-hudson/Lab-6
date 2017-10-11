e76f662703e279122aa9547958112bc9bf448889
Jonaca-hudson <31194367+Jonaca-hudson@users.noreply.github.com>
2017-10-11 13:51:21 -0400

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+            string input;
+
+            bool repeat = true;
+            while (repeat == true)
+            {
+                try
+                {
+                    Console.Write("Enter a word to translate:");
+                    input = Console.ReadLine();
+                    input = input.ToLower();
+
+                    if (input.Length < 3)
+                    {
+                        Console.WriteLine("Please input a word of 3 letters or more");
+                    }
+                  
+                    const string vowels = "aeiouAEIOU";
+                    List<string> sentence = new List<string>();
+
+                    foreach (string word in input.Split(' '))
+                    {
+                        string pig1 = input.Substring(0, 2);
+                        string pig2 = input.Substring(1, input.Length-1);
+                        int letter = vowels.IndexOf(input);
+
+                        if (letter == -1)
+                        {
+                            Console.WriteLine(pig2 + pig1 + "ay");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(word + "way");
+                        }
+                    }
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine("Please use real letters only");
+                }
+                
+                repeat = Continue();
+            }
+        }
+        public static bool Continue()
+        {
+            Console.WriteLine("Do you wish to Continue? y/n");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+            return goOn;
+        }
+    }
+}
